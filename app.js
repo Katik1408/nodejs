7a0e82baf028af6fc3c91fe34d85778c830e1c48
Karthik Saxena <katik.ks@gmail.com>
2021-01-28 10:19:39 +0530

    Added Mongoose

--- app.js
+++ app.js
@@ -1,27 +1,68 @@
 const express = require("express");
-const logger = require('./middleware/logging');
+const logger = require("./middleware/logging");
 app = express();
-const students = require('./routes/students');
+const students = require("./routes/students");
+const mongoose = require("mongoose");
 
+//type Mongoose = typeof mongoose
 
-app.set('view engine','pug');
- app.set('views','./views');
+mongoose
+  .connect("mongodb://localhost/demodb", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connection Successfull");
+  })
+  .catch((err) => console.log(err));
 
+const studentSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  age: { type: Number, required: true },
+  place: { type: String, required: true },
+});
+
+/**
+   
+     Collection Name ------- Model Name
+          students  	---- 	Student
+   */
+
+const Student = mongoose.model("Student", studentSchema);
+
+const newStudentDoc = new Student({
+  name: "Rahul",
+  age: 22,
+  place: "Delhi",
+});
+
+newStudentDoc
+  .save()
+  .then(() => console.log("Inserted Successfully"))
+  .catch((err) => console.log(err));
+
+app.set("view engine", "pug");
+app.set("views", "./views");
+app.set("hello", "Now this is new value");
 
 app.use(express.json());
 app.use(logger);
-app.use(express.static('public'));
-app.use('/api/students',students);
+app.use(express.static("public"));
+app.use("/api/students", students);
 
 app.get("/", (req, res) => {
-
-  res.render('index',{
-    title:'ExpressApp',
-    message: 'Hello from Pug'
+  res.render("index", {
+    title: "ExpressApp",
+    message: "Hello from Pug",
   });
-
-
   res.end();
 });
 
-app.listen(3000);
+app.get("/h1", (req, res) => {
+  res.send(app.get("hello"));
+});
+
+app.listen(3000, () => {
+  console.log("Listeing on port 3000");
+});
+
